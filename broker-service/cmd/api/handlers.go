010aea932f7808c84569c93e726b0049cbd9b3c3
stefan <stefan@dev.projmgmt.de>
2024-08-13 01:12:29 +0200

    ch 15 changes (helper functions)

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -18,9 +10,13 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit the broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	fmt.Println(out)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJson(w, http.StatusOK, payload)
+
+	/*
+		 	out, _ := json.MarshalIndent(payload, "", "\t")
+			fmt.Println(out)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusAccepted)
+			w.Write(out)
+	*/
 }
